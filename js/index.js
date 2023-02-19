ba07c0cd2a6b5c41e24b11fe14a54f4fe46d373d
SpaceVasyl <109691084+SpaceVasyl@users.noreply.github.com>
2023-02-19 23:13:59 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,9 +1,8 @@
 
 
 
-function fetchWeather() {
-    return fetch(`http://api.weatherapi.com/v1/current.json?key=be66b75c9aaf490dbaf162029231902&q=London&aqi=no`)
-    .then((res)=> res.json)
-    .catch((error)=> Notiflix.Notify.error("Oops, something gone wrong"));
+async function fetchWeather(query) {
+    return await fetch(`http://api.weatherapi.com/v1/current.json?key=be66b75c9aaf490dbaf162029231902&q=London&aqi=no`);
   }
 
+const query = '';
