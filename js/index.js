c0e184e5494c668bbe117401c07c6d492ac6c8ba
SpaceVasyl <109691084+SpaceVasyl@users.noreply.github.com>
2023-02-19 18:38:25 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,9 +1,10 @@
 const apiKey = 'ad7a9e4a75ffbc2d7dd9d4572f7e9b33';
 const query = 'https://api.openweathermap.org/data/3.0/onecall';
-const weather = function fetchWeather(query) {
-    return fetch(`https://api.openweathermap.org/data/3.0/onecall?lat={33.44}&lon={-94.04}&exclude={current}&appid=ad7a9e4a75ffbc2d7dd9d4572f7e9b33`)
-    .then((res)=> res.json())
-    .then((res)=>console.log(res))
+
+
+function fetchWeather() {
+    return fetch(`http://api.weatherapi.com/v1/current.json?key=be66b75c9aaf490dbaf162029231902&q=London&aqi=no`)
+    .then((res)=> res.json)
     .catch((error)=> Notiflix.Notify.error("Oops, something gone wrong"));
   }
 
