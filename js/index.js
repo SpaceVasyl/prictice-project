9b12cf9bef8e9489e67f78d46300c8e507b175bd
SpaceVasyl <109691084+SpaceVasyl@users.noreply.github.com>
2023-02-20 02:21:05 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,8 +1,6 @@
 
 
-
-async function fetchWeather(query) {
-    return await fetch(`http://api.weatherapi.com/v1/current.json?key=be66b75c9aaf490dbaf162029231902&q=London&aqi=no`);
+const query = "London";
+const j = async function fetchWeather(query) {
+    return await fetch(`http://api.weatherapi.com/v1/current.json?key=be66b75c9aaf490dbaf162029231902&q=${query}&aqi=no`);
   }
-
-const query = '';
