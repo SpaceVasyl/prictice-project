e4be2f669c433df85512811f08fe308f10f0efa6
SpaceVasyl <109691084+SpaceVasyl@users.noreply.github.com>
2023-03-06 00:30:46 +0200

    ф

--- js/index.js
+++
@@ -1,6 +0,0 @@
-
-
-const query = "London";
-const j = async function fetchWeather(query) {
-    return await fetch(`http://api.weatherapi.com/v1/current.json?key=be66b75c9aaf490dbaf162029231902&q=${query}&aqi=no`);
-  }
